5ae5ab08990be445bf594b3d99be183182c96238
fadhil-amree <84299049+fadhil-amree@users.noreply.github.com>
2021-07-26 07:04:33 +0700

    Update P03.21100924.03.py
    pelengkapan solusi

--- P03.21100924.03.py
+++ P03.21100924.03.py
@@ -1,13 +1,18 @@
-# NIM / Nama  : Muhammad Fadhil Amri
-# Tanggal     : Rabu, 28 April 2021 
-# Deskripsi   : Program menentukan berapa kali kata tuan muncul
+#Nama/NIM:Muhammad Fadhil Amri / 21100924
+#Tanggal:26 Juli 2021
+#Program menghitung banyak kemunculan kata 'tuan'
+c = 0
+hrf = input()
 
-N = int(input('Masukkan nilai N : '))
-kata = input('Masukkan string : ')
-kemunculan = 0
+for i in range(len(hrf)):
+    if hrf[i] =='t':
+        for j in range(i+1,len(hrf)):
+            if hrf[j]=='u':
+                for k in range(j+1,len(hrf)):
+                    if hrf[k]=='a':
+                        for l in range(k+1,len(hrf)):
+                            if hrf[l]=="n":
+                                c+=1
 
-for i in range (len(kata)):
-    for j in range (len(kata)):
-        kata[i] = ''
-        
-    
+
+print(f'Ada {c} buah kemunculan')
